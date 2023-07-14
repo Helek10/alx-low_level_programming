9690fd22f70bb5c3d3bbaf896a2647b2e6e7016e
Helek <herony2010@gmail.com>
2023-07-14 15:57:58 -0700

    Print Alphas

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * *Prints the Alphabet
+ *
+ * Use putchar function to do the printing 
+ */
+int main(void)
+{
+char  alp[52]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      int i;
+
+      for(i=0;i<52;i++) 
+      {
+	putchar(alp[i]);
+      }
+      putchar('\n');
+      return (0);
+}
