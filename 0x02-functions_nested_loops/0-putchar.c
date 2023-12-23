6c4664b286f91a0b275559b5e95f031ca3997530
Helek <herony2010@gmail.com>
2023-12-23 15:11:37 -0800

    10.Triangles

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
-
 /**
- *main - Entry point
- *
- * Return: nothing, return void
+ * main - Entry point
+ * Description: print _putchar follwed by a new line
+ * Return: 0 (success)
  */
-
 int main(void)
 {
-	_putchar('_pu');
-	_putchar('tch');
-	_putchar('ar');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
